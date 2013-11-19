f319339e6924258d6026b451bb35f201b448c439
skandebaba <johann.padkos@gmail.com>
2013-11-20 11:38:10 +1300

    Added Input
    Added input functionality

--- lib/guide.rb
+++ lib/guide.rb
@@ -20,10 +20,31 @@ class Guide
 	def launch!
 		introduction
 		# action loop
-		#   what do you want to do? (list, find, add, quit)
-		#   do that action
+		result = nil
 		# repeat until user quits
+		until result == :quit do
+			#   what do you want to do? (list, find, add, quit)
+			print "> "
+			user_response = gets.chomp
+			#   do that action
+			result = do_action(user_response)
+		end
 		conclusion
+	end
+	
+	def do_action(action)
+		case action
+		when 'list'
+			puts "Listing..."
+		when 'find'
+			puts "Finding..."
+		when 'add'
+			puts "Adding..."
+		when 'quit'
+			return :quit
+		else
+			puts "\nI don't understand that command\n"
+		end
 	end
 	
 	def introduction
