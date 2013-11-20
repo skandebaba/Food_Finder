097c963f16ac51b46f3f174fbf6aafc6174162ac
skandebaba <johann.padkos@gmail.com>
2013-11-20 19:30:24 +1300

    Added find functionality

--- lib/guide.rb
+++ lib/guide.rb
@@ -29,8 +29,8 @@ class Guide
 		result = nil
 		# repeat until user quits
 		until result == :quit do
-			action = get_action
-			result = do_action(action)
+			action, args = get_action
+			result = do_action(action, args)
 		end
 		conclusion
 	end
@@ -42,17 +42,19 @@ class Guide
 			puts "Actions: " + Guide::Config.actions.join(", ") if action
 			print "> "
 			user_response = gets.chomp
-			action = user_response.downcase.strip
+			args = user_response.downcase.strip.split(' ')
+			action = args.shift
 		end
-		return action
+		return action, args
 	end
 	
-	def do_action(action)
+	def do_action(action, args=[])
 		case action
 		when 'list'
 			list
 		when 'find'
-			puts "Finding..."
+			keyword = args.shift
+			find(keyword)
 		when 'add'
 			add
 		when 'quit'
@@ -76,6 +78,23 @@ class Guide
 			puts "\nRestaurant Added\n"
 		else
 			puts "\nSave Error - Restaurant not added\n"
+		end
+	end
+	
+	def find(keyword="")
+		output_action_header("Find a restaurant")
+		
+		if keyword
+			restaurants = Restaurant.saved_restaurants
+			found = restaurants.select do |rest|
+				rest.name.downcase.include?(keyword.downcase) ||
+				rest.cuisine.downcase.include?(keyword.downcase) ||
+				rest.price.to_i <= keyword.to_i
+			end
+			output_restaurant_table(found)
+		else
+			puts "Find using a key phrase to search the restaurant list."
+			puts "Examples: 'find tamale', 'find Mexican', find mex'\n\n"
 		end
 	end
 	
